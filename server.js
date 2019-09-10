8fcddc70da962ae6b246bd3d8ac478d05a2b26f3
Tomislav Sertic <tsertic5@gmail.com>
2019-09-10 20:09:33 +0200

    make it better

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
+
 require('dotenv').config();
 const app = express();
 
@@ -19,6 +21,16 @@ const port = process.env.PORT || 5000;
 const items = require('./routes/api/items');
 
 app.use('/api/items', items);
+
+//serve static assets if in production
+if ((process.env.NODE_ENV = 'production')) {
+  //set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server started on PORT: ${port}`);
